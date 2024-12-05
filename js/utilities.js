0877cde38d773dee73a506e132aa9e2fe2280fe9
Mahfuz Ahmed <108610927+mahfuzahmed7@users.noreply.github.com>
2024-12-05 20:00:23 +0600

    added cash out function

--- js/utilities.js
+++ js/utilities.js
@@ -12,8 +12,3 @@ function getInputFieldById(id) {
     return inputValue;
 }
 
-function getTextFieldValueById(id) {
-    const textValue = document.getElementById(id).innerText;
-    const textNumber = parseFloat(textValue);
-    return textNumber;
-}
